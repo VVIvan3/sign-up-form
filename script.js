a07e3d9fd421aecc916852918f2c932127e3fbba
VVIvan3 <vanya.vorobiev1@mail.ru>
2024-01-31 19:16:46 +0300

    Added error message for password

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const pwdBtn = document.querySelector('#pwd');
 const repeatBtn = document.querySelector('#repeat');
 const passwordBtns = document.querySelectorAll('.passwordbtn');
+const errorText = document.querySelector('.errorpwd');
 
 passwordBtns.forEach((button) => {
     button.addEventListener('change', () => {
         if (pwdBtn.value !== repeatBtn.value) {
             pwdBtn.setCustomValidity('Passwords do not match');
             repeatBtn.setCustomValidity('Passwords do not match');
+            errorText.classList.remove('hidden');
         } else {
             pwdBtn.setCustomValidity('');
             repeatBtn.setCustomValidity('');
+            errorText.classList.add('hidden');
         }
     });
 });
